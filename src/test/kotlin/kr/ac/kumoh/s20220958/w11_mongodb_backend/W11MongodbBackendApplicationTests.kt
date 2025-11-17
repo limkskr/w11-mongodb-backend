cc54664164136a821e458276d61d0f0e611cef02
limskskr <ksks3164@gmail.com>
2025-11-17 18:58:01 +0900

    Initial commit

---
+++ src/test/kotlin/kr/ac/kumoh/s20220958/w11_mongodb_backend/W11MongodbBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220958.w11_mongodb_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W11MongodbBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
