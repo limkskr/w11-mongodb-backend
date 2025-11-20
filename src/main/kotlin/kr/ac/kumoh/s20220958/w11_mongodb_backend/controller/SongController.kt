2155276944732c598c265766b13b7803396a6548
limskskr <ksks3164@gmail.com>
2025-11-20 14:20:59 +0900

    CORS 오류 수정

--- src/main/kotlin/kr/ac/kumoh/s20220958/w11_mongodb_backend/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220958/w11_mongodb_backend/controller/SongController.kt
@@ -2,6 +2,7 @@ package kr.ac.kumoh.s20220958.w11_mongodb_backend.controller
 
 import kr.ac.kumoh.s20220958.w11_mongodb_backend.model.Song
 import kr.ac.kumoh.s20220958.w11_mongodb_backend.service.SongService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/songs")
+@CrossOrigin(origins = ["http://localhost:5173"]) //프론트엔드 주소 추가
 class SongController(
     private val service: SongService
 ) {
